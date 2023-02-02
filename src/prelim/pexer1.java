a6bd8440f73251d7ad6822ae8f84690cba4e0191
Vinz <2212072@slu.edu.ph>
2023-02-02 08:15:18 +0800

    Initial commit

---
+++ src/prelim/pexer1.java
@@ -0,0 +1,68 @@
+/**
+ * Date: 2/2/2023
+ * Name: Lawrence T. Miguel II
+ *
+ * This class is in compliance to the prelim exercise 1 of  the individual exercises wherein the following is asked for.
+ * Problem
+     * Create a program that will look up a variable number of hostnames/IP addresses:
+     * • The program will ask the name of a host to search and then immediately display the hostnames
+     * and IP addresses of the host given by the user
+     * • The program will then ask the user if he/she wants to search for another host (if yes, repeat the
+     * process above, otherwise, exit the program)
+ *
+ *
+ */
+package prelim;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class pexer1 {
+    public static void main(String[] args) {
+        run();
+    }
+
+    private static void run() {
+        Scanner kbd = new Scanner(System.in);
+        int currentHostNumber = 1; // Stores the current host number
+
+        while (true){
+            // Get the Input Host from the user
+            System.out.print("Host " + currentHostNumber + " - Type IP address/Hostname: ");
+            String inputHost = kbd.nextLine();
+            InetAddress[] add;
+
+            // Try catch for exception and continue to the next iteration if not valid
+            try{
+                add =  InetAddress.getAllByName(inputHost);
+            }catch (UnknownHostException e){
+                System.out.println("Invalid Host, Try again!");
+                continue;
+            }
+
+            currentHostNumber++; // Increment the current host as this is valid host
+
+            // Show the number of addresses found for the host
+            System.out.print("    Number of Hosts/IPs: " + add.length); // Get the number by the length of the array
+
+            // Header prints
+            System.out.println();
+            System.out.println("    Host name       Ip Address: ");
+
+            // Iterate through all addresses
+            for (InetAddress address: add){
+                    System.out.println("    "+inputHost+"       "+ address.getHostAddress()); // Show by the function
+            }
+
+            // Ask if Search for another Host
+            System.out.print("Search another [y/n]? ");
+            if (kbd.nextLine().toLowerCase().charAt(0) == 'n') {
+                return; // return to the main method
+            }
+            System.out.println();
+        }
+    }
+
+
+}
